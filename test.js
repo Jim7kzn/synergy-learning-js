fd8c5c97f5ffcbdcf58cfe1d2e845d3dfda9f28f
Jim7rfp <Jim7fp@gmail.com>
2022-09-15 17:16:24 +0300

    2022-09-15. JS. урок 5

--- test.js
+++ test.js
@@ -1,4 +1,17 @@
-let myValn = 3;
-let myVals = "3";
+let firstNum = 7;
+let secondNum = -3;
+firstNum = --firstNum;
+resNum = --secondNum;
+sumNum = firstNum ** (3 - 1) + resNum;
+console.log(sumNum);
 
-console.log(myVals + myValn);
+let myAge = 58;
+if (myAge === 33) {
+	console.log('Вы угадали!!!')
+}
+else if (myAge > 33) {
+	console.log('Увы... время неумолимо.')
+}
+else {
+	console.log('Вы мне льстите!')
+}
