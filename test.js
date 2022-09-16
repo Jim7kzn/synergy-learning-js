fb3b2ea8c62102e83fddff358a212c5623f98172
Jim7rfp <Jim7fp@gmail.com>
2022-09-16 14:38:56 +0300

    2022-09-16. JS. урок 11

--- test.js
+++ test.js
@@ -1,39 +1,10 @@
-// let car = {
-// 	type: 'Toyota',
-// 	model: 'Raum',
-// 	netto: 700,
-// 	maxSpeed: 170,
-// 	aboutCar: function () {
-// 		return 'Это автомобиль';
-// 	},
-// };
+let symId = Symbol();
 
-// let car = {
-// 	type: 'Toyota',
-// 	model: 'Raum',
-// 	netto: 700,
-// 	maxSpeed: 170,
-// 	aboutCar: function () {
-// 		return 'Это автомобиль марки ' + this.type + ' модель ' + this.model;
-// 	}
-// }
-
-function Cars(type = 'Toyota',
-			  model = 'Raum',
-			  netto = 700,
-			  maxSpeed = 170) {
-	this.type = type;
-	this.model = model;
-	this.netto = netto;
-	this.maxSpeed = maxSpeed;
-	this.aboutCar = function () {
-		return 'Это автомобиль \"' + this.type + '\" модели \"' + this.model +
-			'\".\nВес - ' + this.netto + 'кг, максимальная скорость - ' + this.maxSpeed + ' км/ч.\n';
-	}
+user = {
+	name: 'XXX',
+	age: 11,
+	[symId]: 73,
 }
 
-let newCar1 = new Cars();
-let newCar2 = new Cars('Audi', 'Q7', 1000, 220);
+console.log(user[symId]);
 
-console.log(newCar1.aboutCar());
-console.log(newCar2.aboutCar());
