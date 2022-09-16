e41c5fb8627cd5afe592474c7f07b48755344a0b
Jim7rfp <Jim7fp@gmail.com>
2022-09-16 10:52:04 +0300

    2022-09-15. JS. урок 8

--- test.js
+++ test.js
@@ -1,33 +1,15 @@
-function changeVal() {
-	cStr = 'Я люблю тебя, Жизнь!';
+/*  Программа для вычисления факториала числа/
+	определяем функцию, в аргументе одна переменная
+	приводим к числовому виду,
+	задаём значение по умолчанию.
+ */
+let myFactorial = function (nVal = 1) {
+	let res = 1; 	 				// задаём начальное значение
+
+	for(nVal; nVal >= 2; nVal--)	// цикл перебора значений
+		res *= nVal; 				// последовательное умножение
+
+	return res;		 	 			// возвращаем результат.
 }
 
-function myPow(a = 0, b = 1) {
-	return a ** b;
-}
-
-function myFactorial(x= 1) {
-	let result = 1;
-	let i = 1;
-	do {
-		result *= i;
-		i++
-	} while (i <= x)
-	return result;
-}
-
-
-let cStr = 'I love this game!';
-
-console.log(cStr);
-changeVal();
-console.log(cStr);
-
-console.log('-------');
-console.log(myPow(7,3));
-console.log(myPow(11,4));
-
-console.log('-------');
-console.log(myFactorial(2));
-console.log(myFactorial(3));
-console.log(myFactorial(6));
+console.log(myFactorial(6)); 	//выводим значение в терминал.
