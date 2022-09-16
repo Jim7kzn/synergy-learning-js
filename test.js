c6c43dcadb6122cc2dee7526332bf28d90d70b90
Jim7rfp <Jim7fp@gmail.com>
2022-09-16 12:00:59 +0300

    2022-09-15. JS. урок 9

--- test.js
+++ test.js
@@ -1,15 +1,38 @@
-/*  Программа для вычисления факториала числа/
-	определяем функцию, в аргументе одна переменная
-	приводим к числовому виду,
-	задаём значение по умолчанию.
- */
-let myFactorial = function (nVal = 1) {
-	let res = 1; 	 				// задаём начальное значение
-
-	for(nVal; nVal >= 2; nVal--)	// цикл перебора значений
-		res *= nVal; 				// последовательное умножение
-
-	return res;		 	 			// возвращаем результат.
+let warehouse = {
+	sportsNutrition: {
+		proteins: 10,
+		aminoAcids: 15,
+		fatBurners: 5,
+	},
+	dumbbells: 20,
+	balls: 20,
 }
 
-console.log(myFactorial(6)); 	//выводим значение в терминал.
+warehouse.vitamins = 30;
+
+console.log(warehouse);
+
+console.log(warehouse.balls);
+
+warehouse.sportsNutrition.fatBurners = 0;
+console.log(warehouse);
+delete warehouse.sportsNutrition.fatBurners;
+console.log(warehouse);
+
+console.log('proteins' in warehouse.sportsNutrition);
+
+let sum = 0;
+for (key in warehouse) {
+	if (typeof warehouse[key] === 'object') {
+		let sumObj = 0;
+		for (keyObj in warehouse[key]) {
+			sumObj += warehouse[key][keyObj];
+		}
+		sum += sumObj;
+
+	} else {
+		sum += warehouse[key];
+	}
+}
+console.log(warehouse);
+console.log(sum);
