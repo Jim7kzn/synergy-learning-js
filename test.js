7382fd72ad17ed5823aa320bf19e4e3edc3fce0a
Jim7rfp <Jim7fp@gmail.com>
2022-09-16 18:01:51 +0300

    2022-09-16. JS. урок 14

--- test.js
+++ test.js
@@ -1,25 +1,33 @@
-// // массивы создаются либо операцией присвоения
-// let array1 = [];
+let myMap = new Map();
+let jim7 = { name: "Jim7" };
+
+myMap.set("1", "str");			// строка в качестве ключа
+myMap.set(1, "num");			// цифра как ключ
+myMap.set(true, "bool");		// булево значение как ключ
+myMap.set(jim7, 73);			// объект как ключ
+
+// console.log(myMap.get(1));
+// console.log(myMap.get("1"));
+// console.log(myMap.get(jim7));
 //
-// // либо при помощи конструктора
-// let array2 = new Array();
+// console.log(myMap);
+// console.log(myMap.size);		// 4
 
-let myArray = ['111', '222', '333', '444', ];
+let mySet = new Set();
+let name1 = {name: 'JimB', age: 40}
+let name2 = {name: 'Anna', age: 23}
+let name3 = {name: 'Helga', age: 30}
+let name4 = {name: 'JimB', age: 44}
 
-console.log(myArray);
+mySet.add(23);
+mySet.add(1);
+mySet.add('23');
+mySet.add('23232323');
+mySet.add(name1);
+mySet.add(name2);
+mySet.add(name3);
+mySet.add(name4);
+mySet.add(name3);
 
-//добавление элемента в конец массива
-myArray.push('Новый элемент');
-console.log(myArray);
+console.log(mySet);
 
-//добавление элемента в начало массива
-myArray.unshift('Совсем новый элемент');
-console.log(myArray);
-
-//удаляет последний элемент массива
-myArray.pop();
-console.log(myArray);
-
-//удаляет первый элемент массива
-myArray.shift();
-console.log(myArray);
