c67175f8fbb7f34bfa41c74137febcc8d3629d6f
Jim7rfp <Jim7fp@gmail.com>
2022-09-16 15:26:52 +0300

    2022-09-16. JS. урок 12

--- test.js
+++ test.js
@@ -1,10 +1,25 @@
-let symId = Symbol();
+// // массивы создаются либо операцией присвоения
+// let array1 = [];
+//
+// // либо при помощи конструктора
+// let array2 = new Array();
 
-user = {
-	name: 'XXX',
-	age: 11,
-	[symId]: 73,
-}
+let myArray = ['111', '222', '333', '444', ];
 
-console.log(user[symId]);
+console.log(myArray);
 
+//добавление элемента в конец массива
+myArray.push('Новый элемент');
+console.log(myArray);
+
+//добавление элемента в начало массива
+myArray.unshift('Совсем новый элемент');
+console.log(myArray);
+
+//удаляет последний элемент массива
+myArray.pop();
+console.log(myArray);
+
+//удаляет первый элемент массива
+myArray.shift();
+console.log(myArray);
