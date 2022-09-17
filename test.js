d348e1e42f5307970e29b05496e9a0ff31887851
Jim7rfp <Jim7fp@gmail.com>
2022-09-17 16:22:24 +0300

    2022-09-17. JS. урок 21

--- test.js
+++ test.js
@@ -1,24 +1,12 @@
-class NewClas {
-	constructor(val1 = 'defaultValue1', val2= 'defaultValue2') {
-		this.property1 = val1;
-		this.property2 = val2;
-	}
-
-	method1() {console.log(`${this.property1} и ---`)}
-
-	method2() {console.log(`--- и ${this.property2}`)}
-
-	method3() {console.log(`${this.property1} и ${this.property2}`)}
+function first(){
+	// Как будто бы запрос к API
+	setTimeout( function(){
+		console.log(1);
+	}, 500 );
+}
+function second(){
+	console.log(2);
 }
 
-let newObj1 = new NewClas();
-console.log(newObj1);
-newObj1.method1();
-newObj1.method2();
-newObj1.method3();
-
-let newObj2 = new NewClas('Объект 2');
-console.log(newObj2);
-newObj2.method1();
-newObj2.method2();
-newObj2.method3();
+first();
+second();
